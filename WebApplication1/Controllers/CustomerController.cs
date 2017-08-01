22b644dc9deb997681850c5c6401fc2653f43129
atta <atta@SMTX-ATTARASUL>
2017-08-01 16:16:59 +0500

    asdasd

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class CustomerController : Controller
+    {
+        smsEntities db = new smsEntities();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Customer model,string returnUrl)
+        {
+            var c = db.Customers.Where(a => a.Email == model.Email && a.Password == model.Password).First();
+            if (c != null)
+            {
+                FormsAuthentication.SetAuthCookie(model.Email, false);
+                //if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                //    && returnUrl.StartsWith("//") && returnUrl.StartsWith("/\\"))
+                //{
+                //    return Redirect(returnUrl);
+                //}
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid User Name");
+                return View();
+            }
+        }
+    }
+}
